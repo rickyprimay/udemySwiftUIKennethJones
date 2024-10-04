e0b3936334c5151827384f736c7698e3e6438273
Ricky Primayuda Putra <113336156+rickyprimay@users.noreply.github.com>
2024-10-05 01:18:42 +0700

    Initial Commit

---
+++ VacationVegas/VacationVegasApp.swift
@@ -0,0 +1,17 @@
+//
+//  VacationVegasApp.swift
+//  VacationVegas
+//
+//  Created by Ricky Primayuda Putra on 05/10/24.
+//
+
+import SwiftUI
+
+@main
+struct VacationVegasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ VacationVegasTests/VacationVegasTests.swift
@@ -0,0 +1,17 @@
+//
+//  VacationVegasTests.swift
+//  VacationVegasTests
+//
+//  Created by Ricky Primayuda Putra on 05/10/24.
+//
+
+import Testing
+@testable import VacationVegas
+
+struct VacationVegasTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
